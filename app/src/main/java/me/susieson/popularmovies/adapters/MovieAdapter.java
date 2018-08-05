c72ac35235c65c1b4b266941e998161203ec65ce
susie-son <susieson99@gmail.com>
2018-08-05 00:13:39 -0400

    Add polish.

--- app/src/main/java/me/susieson/popularmovies/adapters/MovieAdapter.java
+++ app/src/main/java/me/susieson/popularmovies/adapters/MovieAdapter.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
@@ -60,13 +61,18 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
         @BindView(R.id.movie_poster_image)
         ImageView mImageView;
 
+        @BindView(R.id.movie_title)
+        TextView mTitle;
+
         ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
 
         void bind(final int position, final OnItemClickListener onItemClickListener) {
-            final String URL = ImageUtils.buildUrl(mMovieArrayList.get(position).getPosterPath());
+            Movie movie = mMovieArrayList.get(position);
+            final String URL = ImageUtils.buildUrl(movie.getPosterPath());
+            mTitle.setText(movie.getTitle());
 
             mImageView.setOnClickListener(new View.OnClickListener() {
                 public void onClick(View view) {
