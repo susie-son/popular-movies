a4efbc3f33a52ed855b93ed58adb7b441a369a7b
susie-son <susieson99@gmail.com>
2018-08-08 17:59:18 -0400

    Create generic MovieApiResponse.

--- app/src/main/java/me/susieson/popularmovies/MainActivity.java
+++ app/src/main/java/me/susieson/popularmovies/MainActivity.java
@@ -30,7 +30,7 @@ import me.susieson.popularmovies.adapters.MovieAdapter;
 import me.susieson.popularmovies.database.MovieDatabase;
 import me.susieson.popularmovies.interfaces.OnItemClickListener;
 import me.susieson.popularmovies.models.Movie;
-import me.susieson.popularmovies.models.MovieResponse;
+import me.susieson.popularmovies.models.MovieApiResponse;
 import me.susieson.popularmovies.models.MovieViewModel;
 import me.susieson.popularmovies.network.GetMovieData;
 import me.susieson.popularmovies.network.RetrofitClientInstance;
@@ -54,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
 
     private ArrayList<Movie> mMovieArrayList;
     private MovieAdapter mMovieAdapter;
-    private Callback<MovieResponse> mCallback;
+    private Callback<MovieApiResponse<Movie>> mCallback;
     private SharedPreferences mSharedPreferences;
     private MovieDatabase mMovieDatabase;
     private LiveData<List<Movie>> mLiveDataMovies;
@@ -104,10 +104,10 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
         MovieViewModel movieViewModel = ViewModelProviders.of(this).get(MovieViewModel.class);
         mLiveDataMovies = movieViewModel.getFavoriteMovies();
 
-        mCallback = new Callback<MovieResponse>() {
+        mCallback = new Callback<MovieApiResponse<Movie>>() {
             @Override
-            public void onResponse(@NonNull Call<MovieResponse> call,
-                    @NonNull Response<MovieResponse> response) {
+            public void onResponse(@NonNull Call<MovieApiResponse<Movie>> call,
+                    @NonNull Response<MovieApiResponse<Movie>> response) {
                 hideProgressLoading();
 
                 mMovieArrayList = response.body().getResults();
@@ -128,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
             }
 
             @Override
-            public void onFailure(@NonNull Call<MovieResponse> call, @NonNull Throwable t) {
+            public void onFailure(@NonNull Call<MovieApiResponse<Movie>> call, @NonNull Throwable t) {
                 showErrorMessage();
             }
         };
@@ -257,7 +257,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
                     mLiveDataMovies.removeObserver(mObserver);
                     GetMovieData getMovieData = RetrofitClientInstance.getRetrofitInstance().create(
                             GetMovieData.class);
-                    Call<MovieResponse> call = getMovieData.getMostPopularMovies(
+                    Call<MovieApiResponse<Movie>> call = getMovieData.getMostPopularMovies(
                             BuildConfig.TMDB_API_KEY);
                     call.enqueue(mCallback);
                     break;
@@ -266,7 +266,7 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
                     mLiveDataMovies.removeObserver(mObserver);
                     GetMovieData getMovieData = RetrofitClientInstance.getRetrofitInstance().create(
                             GetMovieData.class);
-                    Call<MovieResponse> call = getMovieData.getTopRatedMovies(
+                    Call<MovieApiResponse<Movie>> call = getMovieData.getTopRatedMovies(
                             BuildConfig.TMDB_API_KEY);
                     call.enqueue(mCallback);
                     break;
